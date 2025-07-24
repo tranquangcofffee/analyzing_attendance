d25a0565743720e46dd3835e3517184b8fb1a2f0
minhncn <minh27122002pro@gmail.com>
2025-07-24 11:59:24 +0700

    group log

--- services/attendance_service.py
+++ services/attendance_service.py
@@ -18,54 +18,42 @@ def process_attendance(df):
     df['full_name'] = df['first_name'].astype(str) + ' ' + df['last_name'].astype(str)
     df['date'] = df['datetime'].dt.date
 
-    # Sắp xếp theo id và datetime để đảm bảo xử lý theo thứ tự thời gian
-    df = df.sort_values(by=['id', 'datetime'])
+    # Sắp xếp theo id, full_name và datetime
+    df = df.sort_values(by=['id', 'full_name', 'datetime'])
 
     records = []
-    # Nhóm theo id và full_name, không nhóm theo date để xử lý ca liên tục
-    grouped = df.groupby(['id', 'full_name'])
+    # Nhóm theo id, full_name và date
+    grouped = df.groupby(['id', 'full_name', 'date'])
 
-    for (emp_id, name), group in grouped:
+    for (emp_id, name, date), group in grouped:
         group = group.sort_values(by='datetime')
-        i = 0
-        while i < len(group):
-            fci = group.iloc[i]['datetime']
-            date = fci.date()
-            lco = fci
-            j = i + 1
+        if len(group) == 0:
+            continue
 
-            # Tìm LCO trong khoảng thời gian hợp lý (dưới 16 giờ)
-            while j < len(group):
-                next_time = group.iloc[j]['datetime']
-                if (next_time - fci).total_seconds() / 3600 <= 16:
-                    lco = next_time
-                    j += 1
-                else:
-                    break
+        # Lấy FCI sớm nhất và LCO muộn nhất trong ngày
+        fci = group.iloc[0]['datetime']
+        lco = group.iloc[-1]['datetime']
+        duration = (lco - fci).total_seconds() / 3600
+        shift_type = 'Không hợp lệ'
 
-            duration = (lco - fci).total_seconds() / 3600
-            shift_type = 'Không hợp lệ'
+        # Phân loại ca
+        if duration > 22:
+            shift_type = 'Thông ca'
+        elif 5 <= fci.hour <= 10 and lco.hour < 22 and duration >= 4:
+            shift_type = 'Ca sáng'
+        elif 16 <= fci.hour <= 19 and 4 <= duration < 14:
+            if lco.date() > fci.date() or (lco.date() == fci.date() and lco.hour < 8):
+                shift_type = 'Ca đêm'
 
-            # Phân loại ca
-            if duration >= 22:
-                shift_type = 'Thông ca'
-            elif 5 <= fci.hour <= 10 and lco.hour < 20 and duration >= 4:
-                shift_type = 'Ca sáng'
-            elif 16 <= fci.hour <= 19 and duration >= 4:
-                if lco.date() > fci.date() or lco.hour < 8 and duration >= 11:
-                    shift_type = 'Ca đêm'
-
-            # Ghi lại record
-            records.append({
-                'ID': emp_id,
-                'Họ tên': name,
-                'Ngày': date,
-                'FCI': fci.strftime('%H:%M:%S'),
-                'LCO': lco.strftime('%H:%M:%S'),
-                'Thời lượng (h)': round(duration, 2),
-                'Loại ca': shift_type
-            })
-
-            i = j
+        # Ghi lại record với định dạng ngày và giờ
+        records.append({
+            'ID': emp_id,
+            'Họ tên': name,
+            'Ngày': date,
+            'FCI': fci.strftime('%Y-%m-%d %H:%M:%S'),
+            'LCO': lco.strftime('%Y-%m-%d %H:%M:%S'),
+            'Thời lượng (h)': round(duration, 2),
+            'Loại ca': shift_type
+        })
 
     return pd.DataFrame(records)
